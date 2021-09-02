c68c26a40680cb09feff0faee83d7c33880f627a
yusufamr <50525245+yusufamr@users.noreply.github.com>
2021-09-02 09:45:02 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 9/1/2021 5:38:17 PM
+ *  Author: user
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
